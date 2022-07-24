12e2a1c1aeabb988aae4d00fdbeb9f5dcd6cf005
bzani <ubzani@gmail.com>
2022-07-24 23:55:26 +0100

    spring starter project

---
+++ src/main/java/com/bdpz/labs/SpringLabsJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.bdpz.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabsJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabsJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdpz/labs/SpringLabsJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdpz.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabsJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
